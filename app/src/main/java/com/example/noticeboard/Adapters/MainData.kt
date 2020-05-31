2a345842f71f31136f9c2d11045a9e7d536e2fdf
Dongkey-Dev <Dongkey-Dev@gmail.com>
2020-05-31 22:46:00 +0900

    add README.md

---
+++ app/src/main/java/com/example/noticeboard/Adapters/MainData.kt
@@ -0,0 +1,9 @@
+package com.example.noticeboard.Adapters
+
+data class MainData(
+    val photo: Int,
+    val Title: String,
+    val Creator: String,
+    val PostDate: String,
+    val ViewCount: Int,
+    val PostContents: String?)
