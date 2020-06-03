1468809b63dec1e306d4d475be714f89571791dc
Dongkey-Dev <Dongkey-Dev@gmail.com>
2020-06-04 01:25:44 +0900

    1/10 percent of gson_list version. ref =m.blog.naver.com/PostView.nhn?blogId=cosmosjs&logNo=221208701289&proxyReferer=https:%2F%2Fwww.google.com%2F

--- app/src/main/java/com/example/noticeboard/Adapters/Adapter.kt
+++ app/src/main/java/com/example/noticeboard/Adapters/Adapter.kt
@@ -8,17 +8,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noticeboard.R
+import com.bumptech.glide.*
+import com.bumptech.glide.request.RequestOptions
+import com.example.noticeboard.Post
 import kotlinx.android.synthetic.main.post.view.*
 
-class Adapter(private  val postlist: List<MainData>) : RecyclerView.Adapter<Adapter.ViewHolder>(){
+class Adapter(private  val postlist: JsonObj) : RecyclerView.Adapter<Adapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.post_list, parent, false)
-
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.bindItems
+
         val currentPost = postlist[position]
 
         holder.imageView.setImageResource(currentPost.photo)
@@ -32,7 +36,9 @@ class Adapter(private  val postlist: List<MainData>) : RecyclerView.Adapter<Adap
         }
     }
 
-    override fun getItemCount() = postlist.size
+    override fun getItemCount(): Int{
+        return postlist.result.count()
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val imageView: ImageView = itemView.findViewById(R.id.dongkey)
@@ -40,5 +46,14 @@ class Adapter(private  val postlist: List<MainData>) : RecyclerView.Adapter<Adap
         val post_Creator: TextView = itemView.postCreator
         val post_ViewCount: TextView = itemView.viewCount
         val post_date : TextView = itemView.postDate
+
+        fun bindItems(data : Post){
+            Glide.with(itemView.context).load(data.photo).apply(RequestOptions().override(600,600))
+                .apply(RequestOptions.centerCropTransform()).into(itemView.dongkey)
+            itemView.postTitle.text = data.Title
+            itemView.postCreator.text = data.Creator
+            itemView.postDate.text = data.PostDate
+            itemView.viewCount.text = data.ViewCount.toString()
+        }
     }
 }
